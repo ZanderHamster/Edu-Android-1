29a87162f7d8fdc11638803c562e5efd618a6ddd
ZanderHamster <zanderhamster@yandex.ru>
2016-09-12 01:19:17 +0300

    Реализованна часть функционала

--- app/src/main/java/edu/bstu/iipo/a13ivt1/lekveishvili/MainActivity.java
+++ app/src/main/java/edu/bstu/iipo/a13ivt1/lekveishvili/MainActivity.java
@@ -1,7 +1,14 @@
 package edu.bstu.iipo.a13ivt1.lekveishvili;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                String textView= ((TextView) findViewById(R.id.value_Phone)).getText().toString();
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+textView));
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(intent);
+                break;
+        }
     }
 }
