8cda7b99df15e8942fb339f346cea63dd204c738
ZanderHamster <zanderhamster@yandex.ru>
2016-09-12 20:12:34 +0300

    Реализован механизм Логирования/Переопределены жизненные циклы

--- app/src/main/java/edu/bstu/iipo/a13ivt1/lekveishvili/MainActivity.java
+++ app/src/main/java/edu/bstu/iipo/a13ivt1/lekveishvili/MainActivity.java
@@ -7,16 +7,64 @@ import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "MyLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"onCreate",Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"onStart",Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"onResume",Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"onResume");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"onPause",Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"onStop",Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"onDestroy",Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"onRestart",Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"onRestart");
     }
 
     public void onClick(View view) {
@@ -24,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
         String textView;
         switch (view.getId()){
             case R.id.button:
+                Log.i(TAG,"Сall number");
                 textView= ((TextView) findViewById(R.id.value_Phone)).getText().toString();
                 intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+textView));
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
@@ -38,11 +87,12 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
             case R.id.value_Phone:
-
+                Log.i(TAG,"Dialing a number");
                 textView= ((TextView) findViewById(R.id.value_Phone)).getText().toString();
                 intent = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:"+textView));
                 break;
             case  R.id.value_Email:
+                Log.i(TAG,"Send email");
                 String emailTo = ((TextView) findViewById(R.id.value_Email)).getText().toString();
                 String[] emailToArray = {emailTo};
                 String subject = "Тема сообщения";
